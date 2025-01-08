7745f30c0cc9a51a42807f03ffc850f2c77a746b
eternal-echo <yuanyu@whut.edu.cn>
2025-01-08 21:26:38 +0800

    填写报告

--- os/proc.c
+++ os/proc.c
@@ -36,6 +36,7 @@ void proc_init(void)
 		* LAB1: you may need to initialize your new fields of proc here
 		*/
 		p->time = -1;
+		memset(p->syscall_times, 0, sizeof(p->syscall_times));
 	}
 	idle.kstack = (uint64)boot_stack_top;
 	idle.pid = 0;
